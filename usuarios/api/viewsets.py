2ba7719708d02a910d23715cbcdc2447120891d3
zeguil <zeguilhermelins@hotmail.com>
2022-03-31 11:50:15 -0400

    permissions

--- usuarios/api/viewsets.py
+++ usuarios/api/viewsets.py
@@ -1,6 +1,7 @@
 from usuarios.models import Usuario
 from .serializers import UsuarioSerializer
 #DRF
+from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
 #Django-filter
 from django_filters.rest_framework import DjangoFilterBackend
@@ -10,4 +11,11 @@ class UsuarioViewSet(ModelViewSet):
     serializer_class = UsuarioSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['username']
+    # Permite que apenas usuários autenticados possam usar o metodo GET, PUT e DELETE
+    permission_classes_by_action = {
+        'create': [permissions.AllowAny],
+        'list': [permissions.IsAuthenticated],
+        'update': [permissions.IsAuthenticated],    
+        'destroy': [permissions.IsAuthenticated]
+    }
     
